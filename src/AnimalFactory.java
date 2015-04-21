3f18e08d7ceefffdc025c77476f173980af51d2a
Jessica Andjani <lukman.jessica1@gmail.com>
2015-04-21 23:16:04 +0700

    animalfactory

--- src/AnimalFactory.java
+++ src/AnimalFactory.java
@@ -7,6 +7,36 @@
  *
  * @author Edwin
  */
+import java.util.*;
+
 public class AnimalFactory {
+    private HashMap<Animal, Collection<Animal>> animalMap = new HashMap<>();
+    Collection<Animal> listAnimal = new ArrayList<>();
+    listAnimal = animalMap.get(Animal);
     
+    void registerAnimal(Class animal) {
+        Method m = animal.getMethod("setId", Integer.class);
+        Sting[] params = null;
+        m.invoke(null, id++);
+    }
+    
+    animalMap.put(Animal, listAnimal);
+    
+    Integer id = 0;
+    void setId (Animal animal) {
+        
+    }
+    
+    
+    public void releaseAnimal(Animal) {
+        
+    }
+   
+    Integer getId() {
+        return id;
+    }
+    
+    public Animal getAnimal(Animal animal){ 
+        AnimalMap.get(animal.getId());
+    }
 }
