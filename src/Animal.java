24c05d04d84df67f0246be4e0a92789f5d81e2a7
edwinwijaya94 <edwinwijaya94@yahoo.com>
2015-04-22 11:28:36 +0700

    fixing Animal class

--- src/Animal.java
+++ src/Animal.java
@@ -9,17 +9,44 @@ import java.awt.Point;
  *
  * @author Edwin
  */
-public interface Animal {
+public abstract class Animal {
     
-    //getter
-    public int getSpeed();
-    public int getSize();
-    public Point getPosition();
-    public EventObserver[] getEventObserver();
-    public String getWord();
+    //attributes
+    public static int id;
+    public int speed=0;
+    public int size=0;
+    public Point position= new Point();
+    public EventObserver[] eventObserver;
+    public String word="";
     
-    //method
-    public void move();
-    public void behaveWord();
-    public void notifyObserver(EventObserver e);
+    
+    //getter, setter
+    public static final int getId(){
+        return id;
+    }
+    
+    public static final void setId(int ID){
+        id = ID;
+    }
+    
+    public final int getSpeed(){
+        return speed;
+    }
+    public final int getSize(){
+        return size;
+    }
+    public  final Point getPosition(){
+        return position;
+    }
+    public  final EventObserver[] getEventObserver(){
+        return eventObserver;
+    }
+    public  final String getWord(){
+        return word;
+    }
+    
+    //abstract method
+    public abstract void move();
+    public abstract void behaveWord();
+    public abstract void notifyObserver(EventObserver e);
 }
