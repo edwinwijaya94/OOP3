df25beea8fbd3f43019dedad3f7eae5bfa8a3800
Jessica Andjani <lukman.jessica1@gmail.com>
2015-04-25 12:02:35 +0700

    Menu Background

--- src/Menu.java
+++ src/Menu.java
@@ -38,12 +38,18 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jPanel1 = new javax.swing.JPanel();
         startNewGameButton = new javax.swing.JButton();
         viewHighScoreButton = new javax.swing.JButton();
         howToPlayButton = new javax.swing.JButton();
         aboutButton = new javax.swing.JButton();
+        exitGameButton = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        BackgroundMenu = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jPanel1.setLayout(null);
 
         startNewGameButton.setText("Start New Game");
         startNewGameButton.setName(""); // NOI18N
@@ -52,41 +58,46 @@ public class Menu extends javax.swing.JFrame {
                 startNewGameButtonActionPerformed(evt);
             }
         });
+        jPanel1.add(startNewGameButton);
+        startNewGameButton.setBounds(360, 190, 120, 23);
+        startNewGameButton.getAccessibleContext().setAccessibleDescription("");
 
         viewHighScoreButton.setText("View High Scores");
+        jPanel1.add(viewHighScoreButton);
+        viewHighScoreButton.setBounds(360, 240, 120, 23);
 
         howToPlayButton.setText("How to Play");
+        jPanel1.add(howToPlayButton);
+        howToPlayButton.setBounds(360, 290, 120, 23);
 
         aboutButton.setText("About");
+        jPanel1.add(aboutButton);
+        aboutButton.setBounds(360, 340, 120, 23);
+
+        exitGameButton.setText("Exit Game");
+        jPanel1.add(exitGameButton);
+        exitGameButton.setBounds(360, 390, 120, 23);
+
+        jLabel1.setFont(new java.awt.Font("Perpetua Titling MT", 1, 30)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(1, 61, 79));
+        jLabel1.setText("TYPER ANIMAL");
+        jPanel1.add(jLabel1);
+        jLabel1.setBounds(300, 110, 250, 50);
+
+        BackgroundMenu.setIcon(new javax.swing.ImageIcon("D:\\Users\\Jessica\\Pictures\\Background\\menu2.png")); // NOI18N
+        jPanel1.add(BackgroundMenu);
+        BackgroundMenu.setBounds(0, 0, 800, 510);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(112, 112, 112)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(viewHighScoreButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(aboutButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(howToPlayButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(startNewGameButton))
-                .addContainerGap(175, Short.MAX_VALUE))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 801, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(46, 46, 46)
-                .addComponent(startNewGameButton)
-                .addGap(36, 36, 36)
-                .addComponent(viewHighScoreButton)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
-                .addComponent(howToPlayButton)
-                .addGap(27, 27, 27)
-                .addComponent(aboutButton)
-                .addGap(70, 70, 70))
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, 510, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
-
-        startNewGameButton.getAccessibleContext().setAccessibleDescription("");
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -131,8 +142,12 @@ public class Menu extends javax.swing.JFrame {
     }
     
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel BackgroundMenu;
     private javax.swing.JButton aboutButton;
+    private javax.swing.JButton exitGameButton;
     private javax.swing.JButton howToPlayButton;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JButton startNewGameButton;
     private javax.swing.JButton viewHighScoreButton;
     // End of variables declaration//GEN-END:variables
